ddf871c1272136c873dd8f348cdc2dfe172677db
Kriti Agarwal <kriti.agarwal4709@gmail.com>
2024-01-26 00:22:52 +0000

    Stripe UI and Hydration warning fix

--- app/api/companion/route.ts
+++ app/api/companion/route.ts
@@ -1,4 +1,5 @@
 import prismadb from "@/lib/prismadb";
+import { checkSubscription } from "@/lib/subscription";
 
 import { currentUser } from "@clerk/nextjs";
 import { NextResponse } from "next/server";
@@ -17,7 +18,11 @@ export async function POST(req: Request) {
             return new NextResponse("Missing required fields", { status: 400 });
         }
 
-        //TODO Check for subscription
+        const isPro = await checkSubscription();
+
+        if (!isPro) {
+            return new NextResponse("Pro subscription required", { status: 403 });
+        }
 
         const companion = await prismadb.companion.create({
             data: {
--- components/navbar.tsx
+++ components/navbar.tsx
@@ -9,31 +9,42 @@ import { cn } from "@/lib/utils";
 import { Button } from "@/components/ui/button";
 import { ModeToggle } from "@/components/mode-toggle";
 import { MobileSidebar } from "./mobile-sidebar";
+import { useProModal } from "@/hooks/use-pro-modal";
 
 const font = Poppins({
-    weight: "600",
-    subsets: ["latin"]
+  weight: "600",
+  subsets: ["latin"],
 });
 
-export const Navbar = () => {
+interface NavbarPro {
+  isPro: boolean;
+};
+
+export const Navbar = ({ isPro }: NavbarPro) => {
+  const proModal = useProModal();
+
   return (
     <div className="fixed w-full z-50 flex justify-between items-center py-2 px-4 border-b border-primary/10 bg-secondary h-16">
       <div className="flex items-center">
         <MobileSidebar />
         <Link href="/">
-          <h1 className={cn(
-                "hidden md:block text-xl md:text-3xl font-bold text-primary",
-                font.className
-            )}>
+          <h1
+            className={cn(
+              "hidden md:block text-xl md:text-3xl font-bold text-primary",
+              font.className
+            )}
+          >
             companion.ai
-        </h1>
+          </h1>
         </Link>
       </div>
       <div className="flex items-center gap-x-3">
-        <Button variant="premium" size="sm">
+        {!isPro && (
+          <Button onClick={proModal.onOpen} variant="premium" size="sm">
             Upgrade
             <Sparkles className="h-4 w-4 fill-white text-white ml-2" />
-        </Button>
+          </Button>
+        )}
         <ModeToggle />
         <UserButton afterSignOutUrl="/" />
       </div>
